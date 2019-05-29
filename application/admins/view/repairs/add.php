1bc9b0cf7ebba98e09ac1f3ba26ab4d72e4da9ae
244103592@qq.com <244103592@qq.com>
2019-05-29 15:46:22 +0800

    完成报修 以及系统设置

--- application/admins/view/repairs/add.php
+++ application/admins/view/repairs/add.php
@@ -4,33 +4,94 @@
     <title></title>
     <link rel="stylesheet" type="text/css" href="/static/plugins/layui/css/layui.css">
     <script type="text/javascript" src="/static/plugins/layui/layui.js"></script>
+    <script type="text/javascript" src="/static/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/static/js/map.jquery.min.js"></script>
+    <style>
+        /* reset */
+        html, body, h1, h2, h3, h4, h5, h6, div, dl, dt, dd, ul, ol, li, p, blockquote, pre, hr, figure, table, caption, th, td, form, fieldset, legend, input, button, textarea, menu {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            padding: 100px;
+            font-size: 14px;
+        }
+
+        h1 {
+            font-size: 26px;
+        }
+
+        p {
+            font-size: 14px;
+            margin-top: 10px;
+        }
+
+        pre {
+            background: #eee;
+            border: 1px solid #ddd;
+            border-left: 4px solid #f60;
+            padding: 15px;
+            margin-top: 15px;
+        }
+
+        h2 {
+            font-size: 20px;
+            margin-top: 20px;
+        }
+
+        .case {
+            margin-top: 15px;
+            width: 400px;
+        }
+
+        .bMap {
+            position: relative;
+        }
+
+        .bMap .map-warp {
+            position: absolute;
+            left: 0;
+            width: 100%;
+            height: 400px;
+            top: 34px;
+            display: none;
+        }
+
+        .bMap input {
+            width: 190px;
+            height: 36px;
+            line-height: 30px;
+            border: 1px solid #d7d7d7;
+        }
+
+        .tangram-suggestion-main {
+            z-index: 9999
+        }
+    </style>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=EZPCgQ6zGu6hZSmXlRrUMTpr"></script>
 </head>
 <body style="padding: 10px;">
 <form class="layui-form">
-<!--    <div class="layui-form-item test"></div>-->
-
     <div class="layui-form-item">
-        <label class="layui-form-label">维修设备</label>
+        <label class="layui-form-label">指定师傅</label>
         <div class="layui-input-block">
-            <input type="radio" name="equipment" value="空调" title="空调" checked="">
-            <input type="radio" name="equipment" value="洗衣机" title="洗衣机">
-            <input type="radio" name="equipment" value="冰箱" title="冰箱">
+            <select name="users" lay-verify="">
+                <option value="0">不指定师傅（等于抢单）</option>
+                {volist name="$users" id="user"}
+                <option value="{$user.id}">{$user.true_name}</option>
+                {/volist}
+            </select>
         </div>
     </div>
     <div class="layui-form-item">
         <label class="layui-form-label">设备品牌</label>
         <div class="layui-input-block">
-            <input type="radio" name="brand" value="美的" title="美的" checked="">
-            <input type="radio" name="brand" value="夏利" title="夏利">
-            <input type="radio" name="brand" value="美玲" title="美玲">
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <label class="layui-form-label">品牌型号</label>
-        <div class="layui-input-block">
-            <input type="radio" name="Version" value="aass" title="aass" checked="">
-            <input type="radio" name="Version" value="AKDJS" title="AKDJS">
-            <input type="radio" name="Version" value="OUXJWS" title="OUXJWS">
+            <select name="brand" lay-verify="">
+                {volist name="$project" id="projec"}
+                <option value="{$projec.id}">{$projec.sort_title}</option>
+                {/volist}
+            </select>
         </div>
     </div>
     <div class="layui-form-item">
@@ -51,17 +112,59 @@
     <div class="layui-form-item">
         <label class="layui-form-label">维修项目</label>
         <div class="layui-input-block">
-            <textarea name="desc" placeholder="请输入内容" class="layui-textarea"></textarea>
+                {volist name="item" id="vo"}
+                <input type="checkbox" name="project" value="{$vo.id}" title="{$vo.maintain_title}" lay-skin="primary">
+                {/volist}
         </div>
     </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">维修描述</label>
+        <div class="layui-input-block">
+            <textarea name="desc" placeholder="请输入内容" id="desc" lay-verify="required" class="layui-textarea"></textarea>
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">详细地址</label>
+        <div class="case">
+            <div class="bMap" id='case3'></div>
+            <div id="callback">
 
-
-
-
-
-
-
-
+            </div>
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">直线距离</label>
+        <div class="layui-input-inline">
+            <input type="text" name="distance" id="distance" value="" placeholder="报修位置和中心点的直线距离" class="layui-input"
+                   style="display: inline-block" disabled="">
+        </div>
+        <div class="layui-input-inline">
+            <input type="text" name="price" id="price" value="" placeholder="预计距离奖励" class="layui-input"
+                   style="display: inline-block" disabled="">
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">联系人姓名</label>
+        <div class="layui-input-inline">
+            <input type="text" name="username" placeholder="请填写客户姓名" class="layui-input">
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">联系人电话</label>
+        <div class="layui-input-inline">
+            <input type="text" name="phone" placeholder="请填写客户姓名" class="layui-input">
+        </div>
+    </div>
+    <div class="layui-form-item">
+        <label class="layui-form-label">故障照片</label>
+        <div class="layui-upload">
+            <button type="button" class="layui-btn" id="test2">图片上传</button>
+            <blockquote class="layui-elem-quote layui-quote-nm" style="margin-top: 10px;">
+                预览图：
+                <div class="layui-upload-list" id="demo2"></div>
+            </blockquote>
+        </div>
+    </div>
 
 
 </form>
@@ -72,46 +175,93 @@
 </div>
 </body>
 </html>
+<script>
+    $(function () {
+        $("#case3").bMap({
+            name: "callback", callback: function (address, point) {
+                $.post('/index.php/admins/repairs/distance', {point: point}, function (res) {
+                    console.log(res);
+                    if (res.code == 0) {
+                        $('#demo2').append('<input type="hidden" name="distance" value="' + res.distance + '">')
+                        $('#demo2').append('<input type="hidden" name="price" value="' + res.price + '">')
+                        $('#distance').val(res.distance)
+                        $('#price').val(res.price)
+                    }
+                }, 'json')
+            }
+        });
+    })
+</script>
 <script type="text/javascript">
-    // layui.config({
-    //     base: '/static/js/',
-    // })
-    layui.use(['layer','form','interact'],function(){
+    layui.use(['layer', 'form', 'upload'], function () {
         var form = layui.form;
         layer = layui.layer;
         $ = layui.jquery;
-        // var interact = layui.interact;
-        // var data=[{:htmlspecialchars_decode($item)}];
-        // interact.render({
-        //     elem : '.test',
-        //     title : '选择联动',
-        //     data : data,
-        //     name : 'sort_title',
-        // });
-        // interact.on('interact(test)',function(data){
-        //     console.dir(data);
-        // })
+        upload = layui.upload;
+        //多图片上传
+        upload.render({
+            elem: '#test2'
+            , url: '/index.php/admins/repairs/uploads'
+            , multiple: true
+            , before: function (obj) {
+                //预读本地文件示例，不支持ie8
+                obj.preview(function (index, file, result) {
+                    $('#demo2').append('<img src="' + result + '" alt="' + file.name + '" class="layui-upload-img" style="width: 92px;">')
+                });
+            }
+            , done: function (res) {
+                console.log(res)
+                $('#demo2').append('<input type="hidden" name="pic" value="' + res.data + '">')
+            }
+        });
     });
 
-    function save(){
+    function save() {
+        var pic = [];
+        var project = [];
+        $("input[name='pic']").each(function(){
+            pic.push($(this).val());
+        })
+        console.log(pic);
         var id = parseInt($('input[name="id"]').val());
-        var maintain_price = $.trim($('input[name="maintain_price"]').val());
-        var maintain_title = $.trim($('input[name="maintain_title"]').val());
-        if(maintain_title==''){
-            layer.alert('项目名称不能为空',{'icon':2});
-            return;
-        }
-        if(maintain_price==''){
-            layer.alert('项目价格不能为空',{'icon':2});
-            return;
-        }
-        $.post('/index.php/admins/abhibition/save',$('form').serialize(),function(res){
-            if(res.code>0){
-                layer.alert(res.msg,{'icon':2});
-            }else{
-                layer.msg(res.msg,{'icon':1});
-                setTimeout(function(){parent.window.location.reload();},1000);
+        var price = $.trim($('input[name="price"]').val());
+        var distance = $.trim($('input[name="distance"]').val());
+        var power =  $("input[name='power']:checked").val();
+        var worry =  $("input[name='worry']:checked").val();
+        var phone = $.trim($('input[name="phone"]').val());
+        var username = $.trim($('input[name="username"]').val());
+        var location_callback = $.trim($('input[name="location_callback"]').val());
+        var callback = $.trim($('input[name="callback"]').val());
+        var desc = $('#desc').val();
+        var brand = $('select[name="brand"]').val();
+        var users = $('select[name="users"]').val();
+        $("input[name='project']:checked").each(function(){
+            project.push($(this).val());
+        })
+
+        $.post('/index.php/admins/repairs/save', {
+            brand:brand,
+            power:power,
+            worry:worry,
+            desc:desc,
+            callback:callback,
+            location_callback:location_callback,
+            username:username,
+            phone:phone,
+            price:price,
+            distance:distance,
+            pic:pic,
+            project:project,
+            uid:users,
+        }, function (res) {
+            if (res.code > 0) {
+                layer.alert(res.msg, {'icon': 2});
+            } else {
+                layer.msg(res.msg, {'icon': 1});
+                setTimeout(function () {
+                    parent.window.location.reload();
+                }, 1000);
             }
-        },'json');
+        }, 'json');
     }
 </script>
--- application/admins/view/repairs/index.php
+++ application/admins/view/repairs/index.php
@@ -5,33 +5,82 @@
     <link rel="stylesheet" type="text/css" href="/static/plugins/layui/css/layui.css">
     <script type="text/javascript" src="/static/plugins/layui/layui.js"></script>
     <style type="text/css">
-        .header span{background: #009688;margin-left: 30px;padding: 10px;color: #ffffff;}
-        .header button{float: right;margin-top: -5px;}
-        .header div{border-bottom: solid 2px #009688;margin-top: 8px;}
+        .header span {
+            background: #009688;
+            margin-left: 30px;
+            padding: 10px;
+            color: #ffffff;
+        }
+
+        .header button {
+            float: right;
+            margin-top: -5px;
+        }
+
+        .header div {
+            border-bottom: solid 2px #009688;
+            margin-top: 8px;
+        }
     </style>
 </head>
 <body style="padding: 10px;">
 <div class="header">
     <span>报修列表</span>
-    <button class="layui-btn layui-btn-sm" onclick="add()">添加</button>
+    <button class="layui-btn layui-btn-sm" onclick="add()">报修创建</button>
     <div></div>
 </div>
 <table class="layui-table">
     <thead>
     <tr>
         <th>ID</th>
-        <th>角色名称</th>
+        <th>维修项目</th>
+        <th>维修地址</th>
+        <th>客户姓名</th>
+        <th>客户电话</th>
+        <th>故障图片</th>
+        <th>创建时间</th>
+        <th>订单状态</th>
         <th>操作</th>
     </tr>
     </thead>
     <tbody>
     {volist name='$lists' id="vo"}
     <tr>
-        <td>{$vo.gid}</td>
-        <td>{$vo.title}</td>
+        <td>{$vo.id}</td>
         <td>
-            <button class="layui-btn layui-btn-warm layui-btn-xs" onclick="add({$vo.gid})">编辑</button>
-            <button class="layui-btn layui-btn-danger layui-btn-xs" onclick="del({$vo.gid})">删除</button>
+            <?php
+            $count = count($vo['project']);
+            $str = '';
+            for ($i = 0; $i <= $count-1; $i++) {
+                $str .= $vo['project'][$i]['maintain_title'].' / ';
+            }
+            echo $str;
+            ?>
+        </td>
+        <td>{$vo.callback}</td>
+        <td>{$vo.username}</td>
+        <td>{$vo.phone}</td>
+        <td>
+            <?php
+            $count = count($vo['pic']);
+            for ($i = 0; $i <= $count-1; $i++) {
+                $pic = $vo['pic'][$i];
+                echo "<img src=' $pic ' style='width: 60px;'>";
+            }
+
+            ?>
+        </td>
+        <td>{:date('Y-m-d',$vo.creat_time)}</td>
+        <td>
+            {switch $vo.status}
+            {case 0}未接单{/case}
+            {case 1}已接单{/case}
+            {default /}已完成
+            {/switch}
+        </td>
+        <td>
+            <button class="layui-btn layui-btn-warm layui-btn-xs" onclick="add({$vo.id})">编辑</button>
+            <button class="layui-btn layui-btn-danger layui-btn-xs" onclick="del({$vo.id})">删除</button>
         </td>
     </tr>
     {/volist}
@@ -40,37 +89,39 @@
 </body>
 </html>
 <script type="text/javascript">
-    layui.use(['layer'],function(){
+    layui.use(['layer'], function () {
         layer = layui.layer;
         $ = layui.jquery;
     });
 
     // 添加
-    function add(gid){
+    function add(gid) {
         var pid = $('#pid').val();
         layer.open({
-            type:2,
-            title:gid>0?'编辑角色':'添加角色',
-            shade:0.3,
-            area:['50%','50%'],
-            content:'/index.php/admins/repairs/add?gid='+gid
+            type: 2,
+            title: gid > 0 ? '编辑角色' : '添加角色',
+            shade: 0.3,
+            area: ['70%', '70%'],
+            content: '/index.php/admins/repairs/add?gid=' + gid
         });
     }
 
     // 删除
-    function del(gid){
-        layer.confirm('确定要删除吗？',{
-            icon:3,
-            btn:['确定','取消']
-        },function(){
-            $.post('/index.php/admins/repairs/delete',{'gid':gid},function(res){
-                if(res.code>0){
-                    layer.alert(res.msg,{'icon':2});
-                }else{
-                    layer.msg(res.msg,{'icon':1});
-                    setTimeout(function(){window.location.reload();},1000);
+    function del(gid) {
+        layer.confirm('确定要删除吗？', {
+            icon: 3,
+            btn: ['确定', '取消']
+        }, function () {
+            $.post('/index.php/admins/repairs/delete', {'gid': gid}, function (res) {
+                if (res.code > 0) {
+                    layer.alert(res.msg, {'icon': 2});
+                } else {
+                    layer.msg(res.msg, {'icon': 1});
+                    setTimeout(function () {
+                        window.location.reload();
+                    }, 1000);
                 }
-            },'json');
+            }, 'json');
         });
     }
 </script>
